c5221e419742a5c4fc0dbdc097a500a1058c8492
Md-shefat-masum <mshefat924@gmail.com>
2023-08-27 20:35:48 +0600

    update issue list

---
+++ sugession.js
@@ -0,0 +1,37 @@
+/**
+    stock alert
+
+    1. available na thakle alert deya hobe
+    2. availabel na holeo order neya jabe, pore confirm kora hobe
+
+    3. branch er jonno order accept korle qty wise payment hobe
+
+    conditions:
+    1. order accept hole stock log e add hobe.
+
+    order cancel korle money refund,
+    stock change,
+    production e date wise status record
+    pricing e qty add add hobe,
+    production cost wise unit price asbe.
+
+    print part add e auto calculation
+
+    todo list, user wise price
+
+    employee name designation
+
+
+    Project: publication skf
+    User: MD Shefat Masum
+    Github: https://github.com/Md-shefat-masum
+    Date: 27 August 2023
+    Time: 17:04:07
+    Path: sugession.js
+    Task: completed tasks
+
+    1. admin pricing management
+    2. add to top product
+    3.
+
+ */
