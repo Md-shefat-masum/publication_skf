001c9a3fbb64ac4f53efa555cdb6f5b2fd8931a5
Md-shefat-masum <mshefat924@gmail.com>
2023-08-29 00:33:50 +0600

    modify paper stock selection

--- resources/js/vue/backend/views/production/orders/Production.vue
+++ resources/js/vue/backend/views/production/orders/Production.vue
@@ -31,18 +31,11 @@
                                     <label for="">Bind Company</label>
                                     <binding-modal :select_qty="1"></binding-modal>
                                 </div>
-                                <div class=" form-group d-grid align-content-start gap-1 mb-2 " >
-                                    <input-field
-                                        :label="`Paper amount <sub>ream</sub>`"
-                                        :name="`paper_amount`"
-                                        :type="`number`"
-                                    />
-                                </div>
+
                                 <div class=" form-group d-grid align-content-start gap-1 mb-2 " >
                                     <input-field
                                         :label="`Print Qty <sub>pcs</sub>`"
                                         :name="`print_qty`"
-                                        :type="`number`"
                                     />
                                 </div>
                                 <div class=" form-group d-grid align-content-start gap-1 mb-2 " >
@@ -55,6 +48,39 @@
                                         <option value="binding">Binding</option>
                                         <option value="complete">Complete</option>
                                     </select>
+                                </div>
+
+                                <div class=" form-group full_width d-grid align-content-start gap-1 mb-2 " >
+                                    <label>Paper Supplier and Paper amount</label>
+                                    <div class="production_supplier_selection_table_body">
+                                        <div class="production_supplier_selection_table">
+                                            <div class="supplier">
+                                                <h5>Supplier</h5>
+                                            </div>
+                                            <div class="stock text-center">
+                                                <h5>Paper amt (rm)</h5>
+                                            </div>
+                                            <div class="action">
+                                                <h5>action </h5>
+                                            </div>
+                                        </div>
+                                        <div v-for="(i, index) in suppliers" :key="i" class="production_supplier_selection_table">
+                                            <div class="supplier">
+                                                <select :name="`paper_supplier[${index}][supplier_paper_id]`" id="" class="form-select">
+                                                    <option v-for="i in get_production_papers.data" :key="i.id" :value="i.id">
+                                                        {{ i.supplier_name }} - {{ i.stock }} rm
+                                                    </option>
+                                                </select>
+                                            </div>
+                                            <div class="stock">
+                                                <input :name="`paper_supplier[${index}][amount]`" value="0" type="text" class="form-control">
+                                            </div>
+                                            <div class="action">
+                                                <a href="" v-if="suppliers.length > 1" @click.prevent="delete_suppliers(index)" class="btn btn-sm btn-outline-danger"><i class="fa fa-trash"></i></a>
+                                                <a href="" @click.prevent="add_suppliers(i+1)" class="btn btn-sm btn-outline-info"><i class="fa fa-plus"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
 
                                 <div class=" form-group full_width d-grid align-content-start gap-1 mb-2 " >
@@ -77,7 +103,7 @@
 </template>
 
 <script>
-import { mapActions, mapMutations } from 'vuex'
+import { mapActions, mapGetters, mapMutations } from 'vuex'
 import InputField from '../../components/InputField.vue'
 import ProductModal from "../products/components/ManagementModal.vue"
 import designerModal from "../designer/components/ManagementModal.vue"
@@ -85,14 +111,16 @@ import printModal from "../prints/components/ManagementModal.vue"
 import bindingModal from "../bindings/components/ManagementModal.vue"
 /** store and route prefix for export object use */
 import PageSetup from './PageSetup';
+import PaperSupplierManagementModal from '../papers/components/ManagementModal.vue';
 const {route_prefix, store_prefix} = PageSetup;
 export default {
-    components: { InputField, ProductModal, designerModal, printModal, bindingModal },
+    components: { InputField, ProductModal, PaperSupplierManagementModal, designerModal, printModal, bindingModal },
     data: function(){
         return {
             /** store prefix for JSX */
             store_prefix,
-            route_prefix
+            route_prefix,
+            suppliers: [1],
         }
     },
     created: function () {
@@ -100,19 +128,33 @@ export default {
         this[`set_clear_selected_production_designers`](false);
         this[`set_clear_selected_production_prints`](false);
         this[`set_clear_selected_production_bindings`](false);
+        this[`set_production_paper_paginate`](1000)
     },
     methods: {
-        ...mapActions([`store_production_production_order`]),
+        ...mapActions([
+            `store_production_production_order`,
+            `fetch_production_papers`,
+        ]),
         ...mapMutations([
             `set_clear_selected_production_products`,
             `set_clear_selected_production_designers`,
             `set_clear_selected_production_prints`,
             `set_clear_selected_production_bindings`,
+            `set_production_paper_paginate`,
         ]),
         call_store: function(name, params=null){
             this[name](params)
         },
+        add_suppliers: function(i){
+            this.suppliers.push(i);
+        },
+        delete_suppliers: function(index){
+            this.suppliers.splice(index,1);
+        },
     },
+    computed: {
+        ...mapGetters(['get_production_papers']),
+    }
 };
 </script>
 
