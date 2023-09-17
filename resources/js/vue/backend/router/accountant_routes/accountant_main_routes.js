f0f3b2d457d34a5d7bf919bb5a5c6697e0a3e571
Md-shefat-masum <mshefat924@gmail.com>
2023-09-17 19:17:56 +0600

    accountant route setup

--- resources/js/vue/backend/router/accountant_routes/accountant_main_routes.js
+++ resources/js/vue/backend/router/accountant_routes/accountant_main_routes.js
@@ -1,6 +1,7 @@
 import AccountantDashboard from "../../views/AccountantDashboard.vue"
 import AccountantLayout from "../../views/AccountantLayout.vue"
 import account_categories from "./sub_routes/account_categories";
+import account_types from "./sub_routes/account_types";
 export default {
     path: "/accountant",
     component: AccountantLayout,
@@ -11,5 +12,6 @@ export default {
             component: AccountantDashboard,
         },
         account_categories,
+        account_types,
     ],
 };
