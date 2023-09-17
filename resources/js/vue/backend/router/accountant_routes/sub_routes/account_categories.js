00bca79089914458b26a0915ba4e9967ae1de383
Md-shefat-masum <mshefat924@gmail.com>
2023-09-17 19:18:05 +0600

    account category setup

--- resources/js/vue/backend/router/accountant_routes/sub_routes/account_categories.js
+++ resources/js/vue/backend/router/accountant_routes/sub_routes/account_categories.js
@@ -1,5 +1,6 @@
 import Layout from "../../../views/accountant/account_categories/Layout";
 import All from "../../../views/accountant/account_categories/All";
+import AllincomeExpense from "../../../views/accountant/account_categories/AllincomeExpense.vue";
 import Create from "../../../views/accountant/account_categories/Create";
 import Edit from "../../../views/accountant/account_categories/Edit";
 import Details from "../../../views/accountant/account_categories/Details";
@@ -10,7 +11,7 @@ export default {
     path: "account-categories",
     component: Layout,
     props: {
-        role_permissions: ["branch"],
+        role_permissions: ["accountant"],
         layout_title: "Account Category Management",
     },
     children: [
@@ -18,6 +19,11 @@ export default {
             path: ``,
             name: `All${prefix}`,
             component: All,
+        },
+        {
+            path: `income-expense`,
+            name: `AccountantAllincomeExpense`,
+            component: AllincomeExpense,
         },
         {
             path: `import`,
