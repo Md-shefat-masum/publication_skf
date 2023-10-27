804c9d52e54bbacb7b9ddd3e6638ca8c84625656
Md-shefat-masum <mshefat924@gmail.com>
2023-10-28 00:42:23 +0600

    create supplier menus

--- resources/js/vue/backend/router/accountant_routes/accountant_main_routes.js
+++ resources/js/vue/backend/router/accountant_routes/accountant_main_routes.js
@@ -3,6 +3,12 @@ import AccountantLayout from "../../views/AccountantLayout.vue"
 import account_categories from "./sub_routes/account_categories";
 import account_types from "./sub_routes/account_types";
 import account_entries from "./sub_routes/account_entries";
+
+import paper from "./sub_routes/paper";
+import print from "./sub_routes/print";
+import binding from "./sub_routes/binding";
+import designer from "./sub_routes/designer";
+
 export default {
     path: "/accountant",
     component: AccountantLayout,
@@ -15,5 +21,10 @@ export default {
         account_categories,
         account_types,
         account_entries,
+
+        paper,
+        print,
+        binding,
+        designer,
     ],
 };
