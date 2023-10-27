6b0b0aec4f4d87212f04388c388460208d6d4dee
Md-shefat-masum <mshefat924@gmail.com>
2023-10-28 00:42:04 +0600

    fix stock issue

--- database/seeders/Product/SupplierPaperStockSeeder.php
+++ database/seeders/Product/SupplierPaperStockSeeder.php
@@ -43,16 +43,17 @@ class SupplierPaperStockSeeder extends Seeder
             "Specialty"
         );
 
-        for ($i = 1; $i <= 10; $i++) {
+        for ($i = 1; $i <= SupplierPaper::count(); $i++) {
             $supplier_paper = SupplierPaper::find($i);
             $supplier_paper_stock = null;
 
             for ($j = 0; $j < 4; $j++) {
+                $cost_per_paper = [.3, .5, 1, 1.5][rand(0, 3)];
                 $supplier_paper_stock = SupplierPaperStock::create([
                     "paper_name" => $paper[$j],
                     "paper_type" => $printing_paper_types[$j],
-                    'cost_per_paper' => rand(.3, 2),
-                    'cost_per_ream' => rand(300, 500),
+                    'cost_per_paper' => $cost_per_paper,
+                    'cost_per_ream' => $cost_per_paper * 500,
                     'supplier_paper_id' => $i,
                     'stock' => rand(100, 200),
                     'purchase_date' => Carbon::now()->subDays(rand(20, 100))->toDateString(),
@@ -60,7 +61,9 @@ class SupplierPaperStockSeeder extends Seeder
                 ]);
             }
 
-            $supplier_paper->stock = $supplier_paper->SupplierPaperStock()->sum('stock') - $supplier_paper->SupplierPaperUsed()->sum('amount');
+            $total_stock = $supplier_paper->SupplierPaperStock()->sum('stock');
+            $used_stock = $supplier_paper->SupplierPaperUsed()->sum('amount');
+            $supplier_paper->stock =  $total_stock - $used_stock;
             $supplier_paper->purchase_date = $supplier_paper_stock->purchase_date;
             $supplier_paper->save();
         }
