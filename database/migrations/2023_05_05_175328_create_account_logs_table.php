a4ae892297635a2a1aa052108ccd4b9c433f8ce1
Md-shefat-masum <mshefat924@gmail.com>
2023-10-29 02:08:48 +0600

    account log migration modify

--- database/migrations/2023_05_05_175328_create_account_logs_table.php
+++ database/migrations/2023_05_05_175328_create_account_logs_table.php
@@ -19,8 +19,12 @@ class CreateAccountLogsTable extends Migration
             $table->timestamp('date')->nullable();
             $table->bigInteger('category_id')->nullable();
             $table->bigInteger('account_id')->default(1);
+            $table->bigInteger('account_number_id')->nullable();
+            $table->string('trx_id',100)->nullable();
+
             $table->tinyInteger('is_expense')->default(0);
             $table->tinyInteger('is_income')->default(0);
+
             $table->float('amount')->nullable();
             $table->text('description')->nullable();
 
