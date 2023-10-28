1b9fd9e9fab43245684148deeca544e5a3ce6cde
Md-shefat-masum <mshefat924@gmail.com>
2023-10-29 03:06:53 +0600

    modify heading title

--- resources/js/vue/backend/views/accountant/bindings/PageSetup.js
+++ resources/js/vue/backend/views/accountant/bindings/PageSetup.js
@@ -1,5 +1,5 @@
 export default {
-    layout_title: 'Binding Management',
+    layout_title: 'Binding Suppliers',
     route_prefix: 'ProductionBinding',
     store_prefix: 'production_binding',
     pagination_limits: [10,5,25,50,100],
