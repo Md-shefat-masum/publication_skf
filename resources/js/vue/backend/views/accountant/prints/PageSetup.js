86fa0b2b646ebf6a805fe56ef5dc9ef781ce99ab
Md-shefat-masum <mshefat924@gmail.com>
2023-10-29 03:07:26 +0600

    fix page title

--- resources/js/vue/backend/views/accountant/prints/PageSetup.js
+++ resources/js/vue/backend/views/accountant/prints/PageSetup.js
@@ -1,5 +1,5 @@
 export default {
-    layout_title: 'Print Management',
+    layout_title: 'Print Suppliers',
     route_prefix: 'ProductionPrinting',
     store_prefix: 'production_print',
     pagination_limits: [10,5,25,50,100],
