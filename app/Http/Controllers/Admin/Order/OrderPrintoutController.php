b793f0c80640269d1b4f49299956907b9d6262e5
Md-shefat-masum <mshefat924@gmail.com>
2023-11-30 04:58:57 +0600

    fit print printout invoice html

--- app/Http/Controllers/Admin/Order/OrderPrintoutController.php
+++ app/Http/Controllers/Admin/Order/OrderPrintoutController.php
@@ -13,6 +13,7 @@ class OrderPrintoutController extends Controller
     {
         $order->details = $order->order_details()->get();
         $order->user = $order->user()->first();
+        $order->paid = $order->order_payments()->where('approved',1)->sum('amount');
         // $order->details = OrderDetails::take(60)->get();
         // dd($order->toArray());
         return view('backend.order_prints.sales_invoice',compact('order'));
