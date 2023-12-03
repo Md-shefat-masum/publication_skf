ce5e2e7b33708689a144a6e98f98949f324abc05
Md-shefat-masum <mshefat924@gmail.com>
2023-12-03 21:34:12 +0600

    retive related user in employee task list

--- resources/js/vue/backend/views/employee/task/TaskList.vue
+++ resources/js/vue/backend/views/employee/task/TaskList.vue
@@ -3,17 +3,18 @@
         <div class="task_list custom_scroll">
             <div v-for="task in all_tasks.data" :key="task.id" class="task_item d-flex gap-2 border rounded mb-2 p-2">
                 <div class="left" style="width: 30px;">
-                    <input :checked="task.complete" type="checkbox" @change="complete_task(task.id)"
+                    <input v-if="task.auth_user" :checked="task.auth_user.is_complete" type="checkbox"
+                        @change="em_complete_task(task.auth_user.id)"
                         class="form-check-input border-info">
                     <br>
                     <br>
-                    <router-link :to="{ name: 'TaskDetailsEmployee', params: { id: task.id } }"
+                    <router-link v-if="task.creator == auth.id" :to="{ name: 'TaskDetailsEmployee', params: { id: task.id } }"
                         class="text-warning text-1xl">
                         <i class="fa fa-pencil"></i>
                     </router-link>
                     <br>
                     <br>
-                    <a href="#" @click.prevent="delete_task(task.id)" class="text-danger text-1xl">
+                    <a v-if="task.creator == auth.id" href="#" @click.prevent="em_delete_task(task.id);" class="text-danger text-1xl">
                         <i class="fa fa-trash"></i>
                     </a>
                 </div>
@@ -37,14 +38,14 @@
             </div>
         </div>
         <div class="pt-2">
-            <a @click.prevent="fetch_tasks(all_tasks.prev_page_url)" class="btn btn-sm btn-info" href="">prev</a>
-            <a @click.prevent="fetch_tasks(all_tasks.next_page_url)" class="btn btn-sm btn-info" href="">next</a>
+            <a v-if="all_tasks.prev_page_url" @click.prevent="employeeFetchAllTasks(all_tasks.prev_page_url)" class="btn btn-sm btn-info" href="">prev</a>
+            <a v-if="all_tasks.next_page_url" @click.prevent="employeeFetchAllTasks(all_tasks.next_page_url)" class="btn btn-sm btn-info" href="">next</a>
         </div>
     </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
     data: function () {
@@ -57,10 +58,22 @@ export default {
         console.log("datatask",this.all_tasks);
     },
     methods: {
-        ...mapActions(['complete_task']),
-        async employeeFetchAllTasks() {
+        ...mapActions({
+            fetch_tasks: 'super_admin_fetch_all_tasks',
+            delete_task: 'delete_task',
+            complete_employee_task: 'complete_employee_task',
+        }),
+        em_complete_task: async function(id){
+            await this.complete_employee_task(id);
+            // await this.employeeFetchAllTasks();
+        },
+        em_delete_task: async function(id){
+            await this.delete_task(id);
+            await this.employeeFetchAllTasks();
+        },
+        async employeeFetchAllTasks(url = 'task/get_all_employee_task') {
             try {
-                const res = await axios.get('task/get_all_employee_task');
+                const res = await axios.get(url);
                 this.all_tasks = res.data;
             } catch (error) {
                 // Handle error appropriately
@@ -68,6 +81,11 @@ export default {
             }
         }
     },
+    computed: {
+        ...mapGetters({
+            auth: `get_auth_information`,
+        }),
+    }
 
 }
 </script>
