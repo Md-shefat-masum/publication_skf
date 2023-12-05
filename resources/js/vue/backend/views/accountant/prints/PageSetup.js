a4d344b482eaed437d5b67e0fbd35832308e2d3b
Md-shefat-masum <mshefat924@gmail.com>
2023-12-05 21:32:14 +0600

    change page prefixes

--- resources/js/vue/backend/views/accountant/prints/PageSetup.js
+++ resources/js/vue/backend/views/accountant/prints/PageSetup.js
@@ -1,6 +1,6 @@
 export default {
     layout_title: 'Print Suppliers',
-    route_prefix: 'ProductionPrinting',
+    route_prefix: 'AccountantPrinting',
     store_prefix: 'production_print',
     pagination_limits: [10,5,25,50,100],
 }
