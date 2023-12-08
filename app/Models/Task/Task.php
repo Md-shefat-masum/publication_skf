75e2ca99cf3878c4a0206b1fd2f9cf4a64f0dee9
Md-shefat-masum <mshefat924@gmail.com>
2023-12-08 21:29:09 +0600

    create accessor of date and time

--- app/Models/Task/Task.php
+++ app/Models/Task/Task.php
@@ -3,6 +3,7 @@
 namespace App\Models\Task;
 
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,10 @@ class Task extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    protected $appends = [
+        'formated_date',
+        'formated_time',
+    ];
 
     public function variants()
     {
@@ -19,5 +24,24 @@ class Task extends Model
     public function users()
     {
         return $this->belongsToMany(User::class,'task_users');
+    }
+
+    public function auth_user()
+    {
+        return $this->hasOne(TaskUser::class,'task_id')->where('user_id',auth()->user()->id);
+    }
+
+    public function given_user()
+    {
+        return $this->hasOne(TaskUser::class,'task_id');
+    }
+
+    public function getFormatedDateAttribute()
+    {
+        return Carbon::parse($this->target_date)->format('d M, Y');
+    }
+    public function getFormatedTimeAttribute()
+    {
+        return Carbon::parse($this->target_time)->format('h:i a');
     }
 }
