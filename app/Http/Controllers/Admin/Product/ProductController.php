a19ca977010defaefa2997cdbab06ef910ef4c26
Md-shefat-masum <mshefat924@gmail.com>
2023-12-16 09:12:57 +0600

    product management

--- app/Http/Controllers/Admin/Product/ProductController.php
+++ app/Http/Controllers/Admin/Product/ProductController.php
@@ -44,7 +44,7 @@ class ProductController extends Controller
         $data = $query->paginate($paginate);
 
         foreach ($data->items() as $item) {
-            $item->final_stock = $item->stock + $item->sales + $item->returns;
+            $item->final_stock = $item->stock - $item->sales + $item->returns;
             $item->is_low_stock = $item->final_stock <= $item->stock_alert_qty? 1: 0;
             // dd($item);
         }
