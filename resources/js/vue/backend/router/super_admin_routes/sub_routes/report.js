f6019bcad59efc1652e828688d21377e6bf87df6
Md-shefat-masum <mshefat924@gmail.com>
2023-12-18 04:08:38 +0600

    report routes

--- resources/js/vue/backend/router/super_admin_routes/sub_routes/report.js
+++ resources/js/vue/backend/router/super_admin_routes/sub_routes/report.js
@@ -3,30 +3,54 @@ import Layout from "../../../views/accountant/reports/Layout";
 import IncomeLedger from "../../../views/accountant/reports/IncomeLedger.vue";
 import ExpenseLedger from "../../../views/accountant/reports/ExpenseLedger.vue";
 import Statements from "../../../views/accountant/reports/Statements.vue";
+import ProductReport from "../../../views/superadmin/reports/ProductReport.vue";
+import AllincomeExpense from "../../../views/common_pages/AllincomeExpense.vue";
+import AllInStockProducts from "../../../views/common_pages/AllInStockProducts.vue";
+import AllOutStockProducts from "../../../views/common_pages/AllOutStockProducts.vue";
 
-let prefix = "AccountantReport"
+let prefix = "Super Admin Report"
 export default {
     path: "report",
     component: Layout,
     props: {
-        role_permissions: ["accountant"],
+        role_permissions: ["super_admin"],
         layout_title: `${prefix} Management`,
     },
     children: [
         {
             path: `income-ledger`,
-            name: `IncomeLedger`,
+            name: `SuperAdminIncomeLedger`,
             component: IncomeLedger,
         },
         {
             path: `expense-ledger`,
-            name: `ExpenseLedger`,
+            name: `SuperAdminExpenseLedger`,
             component: ExpenseLedger,
         },
         {
             path: `statements`,
-            name: `Statements`,
+            name: `SuperAdminStatements`,
             component: Statements,
+        },
+        {
+            path: `all-income-expense`,
+            name: `SuperAllincomeExpense`,
+            component: AllincomeExpense,
+        },
+        {
+            path: `in-stock-products`,
+            name: `SuperAllInStockProducts`,
+            component: AllInStockProducts,
+        },
+        {
+            path: `out-stock-products`,
+            name: `SuperAllOutStockProducts`,
+            component: AllOutStockProducts,
+        },
+        {
+            path: `product-report`,
+            name: `SuperProductReport`,
+            component: ProductReport,
         },
 
     ],
