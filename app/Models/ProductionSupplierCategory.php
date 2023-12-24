13fc449c6d59ce77f34ae6533bac7d4cc92929aa
Md-shefat-masum <mshefat924@gmail.com>
2023-12-24 11:47:10 +0600

    accessor and mutetor

--- app/Models/ProductionSupplierCategory.php
+++ app/Models/ProductionSupplierCategory.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class ProductionSupplierCategory extends Model
 {
     use HasFactory;
+
+    protected $appends = [
+        'show_suppliers',
+        'selected_id',
+        'selected_name',
+    ];
+
+    public function suppliers()
+    {
+        return $this->hasMany(ProductionSupplier::class,'category_id','id');
+    }
+
+    public function getShowSuppliersAttribute()
+    {
+        return false;
+    }
+    public function getSelectedIdAttribute()
+    {
+        return null;
+    }
+    public function getSelectedNameAttribute()
+    {
+        return null;
+    }
 }
